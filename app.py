3dc2ece8786fed06b714ff3f0772c7ab7435d64c
shivamsonari <45078053+shivamsonari@users.noreply.github.com>
2021-09-01 17:09:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -38,6 +38,8 @@ def generate():
 
         #create a new excel file for every sheet
         for name in sheet_names:
+            xlrd.xlsx.ensure_elementtree_imported(False, None)
+            xlrd.xlsx.Element_has_iter = True
             
             if name in sheet :
                 
